a942136e32da429ab063ace9b8cb0019815d90cb
MeowWow520 <Meow114514@outlook.com>
2026-05-08 20:05:57 +0800

    feat: 添加vcpkg工具链 || 初始化CMake

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,11 @@
 // Created by MeowWow520 on 2026/5/8.
 //
 #include <iostream>
+#include <SDL3/SDL.h>
+
+
 
 int main() {
     std::cout << "Hello World!" << std::endl;
+    SDL_Log("SDL_Log: Hello World!");
 }
